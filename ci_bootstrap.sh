004677a86922096391639913db698118f8d84e1f
cidadao <mario.ribas@gmail.com>
2014-11-14 02:38:43 +0000

    ci_bootstrap.sh added and .travis.yml updated

---
+++ ci_bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+ROOT_DIR=/home/$SUDO_USER/
+
+QKTHINGS_DIR=$ROOT_DIR/qkthings
+QKTHINGS_LOCAL=/home/$SUDO_USER
+DEV_DIR=$QKTHINGS_DIR/dev
+TOOLCHAIN_DIR=$QKTHINGS_LOCAL/qkthings_local/toolchain
+
+sudo apt-get install build-essential doxygen
+
+git clone https://github.com/qkthings/qkthings.git
+sudo python qkthings/dev/python/qkthings/toolman.py -t arduino -r $TOOLCHAIN_DIR --dist=linux
+sudo python qkthings/dev/python/qkthings/toolman.py -t efm32 -r $TOOLCHAIN_DIR --dist=linux
+
+git clone https://github.com/qkthings/qkperipheral.git
+git clone https://github.com/qkthings/qkprogram.git
+git clone https://github.com/qkthings/qkdsp.git
+
