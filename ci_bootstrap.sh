3a5a28738f835ddbfe2eb598c06e3c3f660bc0be
cidadao <mario.ribas@gmail.com>
2014-11-14 03:03:39 +0000

    travis ci testing

--- ci_bootstrap.sh
+++ ci_bootstrap.sh
@@ -7,11 +7,13 @@ QKTHINGS_LOCAL=/home/$SUDO_USER
 DEV_DIR=$QKTHINGS_DIR/dev
 TOOLCHAIN_DIR=$QKTHINGS_LOCAL/qkthings_local/toolchain
 
-sudo apt-get install build-essential doxygen
+apt-get install build-essential doxygen
 
 git clone https://github.com/qkthings/qkthings.git
-sudo python qkthings/dev/python/qkthings/toolman.py -t arduino -r $TOOLCHAIN_DIR --dist=linux
-sudo python qkthings/dev/python/qkthings/toolman.py -t efm32 -r $TOOLCHAIN_DIR --dist=linux
+python qkthings/dev/python/setup.py install
+
+python qkthings/dev/python/qkthings/toolman.py -t arduino -r $TOOLCHAIN_DIR --dist=linux
+python qkthings/dev/python/qkthings/toolman.py -t efm32 -r $TOOLCHAIN_DIR --dist=linux
 
 git clone https://github.com/qkthings/qkperipheral.git
 git clone https://github.com/qkthings/qkprogram.git
