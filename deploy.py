10affca99f3eb755643a5f3019549b77831b7668
cidadao <mario.ribas@gmail.com>
2014-11-14 01:48:45 +0000

    Improvements on deploy

--- deploy.py
+++ deploy.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
 
+import argparse
 from os import getcwd, chdir, path, environ
 from subprocess import call
+from qkthings.utils import cmd
+
+targets = [
+	"arduino.uno",
+	"arduino.nano",
+	"efm32.dev_tg"
+]
+
+libs = [
+	"qkperipheral",
+	"qkprogram",
+	"qkdsp"
+]
 
 def unset_all():
 	environ["TARGET"] = ""
@@ -9,41 +23,26 @@ def unset_all():
 	environ["TEST"] = ""
 	environ["APP"] = ""
 
-def deploy():
-	print " ### Deploy embedded"
+def deploy(verbose = False):
+	print "=== Deploy embedded"
 	rootdir = getcwd()
-	devnull = open('/dev/null', 'w')
-
-	targets = []
-	targets.append("arduino.uno")
-	targets.append("arduino.nano")
-#	targets.append("efm32.g_olimex")
-#	targets.append("efm32.dev_tg")
-	targets.append("efm32.dev_tg_revb")
-
-
-	libs = []
-	libs.append("qkprogram")
-	libs.append("qkperipheral")
-	libs.append("qkdsp")
 
 	for target in targets:
 		for lib in libs:
 			chdir(path.join(rootdir,lib))
-			print " === Deploy %s %s" % (target, lib)
-			call(["python", "deploy.py"],stdout=devnull, stderr=devnull)		
-
-	libs = []
-	libs.append("qkprogram")
+			print " > Deploy %s %s" % (target, lib)
+			cmd(["python", "deploy.py"], verbose)		
 
 	for target in targets:
 		for lib in libs:	
 			chdir(rootdir)
 			unset_all()
-			print " === Build %s %s" % (target, lib)
-			call(["make","clean","LIB=%s" % lib,"TARGET=%s" % target], stdout=devnull, stderr=devnull)
-			call(["make","lib","LIB=%s" % lib,"TARGET=%s" % target], stdout=devnull, stderr=devnull)
-
+			print " > Build %s %s" % (target, lib)
+			cmd(["make","clean","LIB=%s" % lib,"TARGET=%s" % target], verbose)
+			cmd(["make","lib","LIB=%s" % lib,"TARGET=%s" % target], verbose)
 
 if __name__ == "__main__":
-	deploy();
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-v","--verbose", action="store_true", default=False, help='verbose')
+	args = parser.parse_args()
+	deploy(args.verbose)
